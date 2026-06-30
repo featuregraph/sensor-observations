9eeef68e02988e9b46926de2ac54d2c31f753e64
Nazia.Habib <nazia.habib@patenergy.com>
2026-06-30 15:14:00 -0500

    load fit and input data

--- src/sensor_observations/observation/load_data.py
+++ src/sensor_observations/observation/load_data.py
@@ -18,3 +18,20 @@ def load_observations(problem):
         table["hidden_state"].to_numpy(),
         table["measurement"].to_numpy(),
     ], axis=1)
+
+def load_measurements(problem):
+    path = os.path.join(
+        os.path.dirname(__file__),
+        "..",
+        "benchmark",
+        problem,
+        "measurements.parquet"
+    )
+    path = os.path.abspath(path)
+
+    table = pq.read_table(path)
+
+    return np.stack([
+        table["state"].to_numpy(),
+        table["measurement"].to_numpy(),
+    ], axis=1)
