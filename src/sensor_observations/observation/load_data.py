828114e98c2322866824ab34b5eff72c3e2fe654
Nazia.Habib <nazia.habib@patenergy.com>
2026-07-01 09:49:50 -0500

    load observations and create prediction pipeline

--- src/sensor_observations/observation/load_data.py
+++ src/sensor_observations/observation/load_data.py
@@ -2,30 +2,13 @@ import os
 import pyarrow.parquet as pq
 import numpy as np
 
-def load_observations(problem):
+def load_observations(problem, dataset):
     path = os.path.join(
         os.path.dirname(__file__),
         "..",
         "benchmark",
         problem,
-        "observations.parquet"
-    )
-    path = os.path.abspath(path)
-
-    table = pq.read_table(path)
-
-    return np.stack([
-        table["hidden_state"].to_numpy(),
-        table["measurement"].to_numpy(),
-    ], axis=1)
-
-def load_measurements(problem):
-    path = os.path.join(
-        os.path.dirname(__file__),
-        "..",
-        "benchmark",
-        problem,
-        "measurements.parquet"
+        f"{dataset}.parquet"
     )
     path = os.path.abspath(path)
 
